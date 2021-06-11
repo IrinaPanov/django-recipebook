9eaa252d631c235755a87be8ade2311ce0962e1a
IrinaPanov <iwell1202@gmail.com>
2021-06-11 12:40:01 -0400

    cache added

--- recipesite/recipesite/settings.py
+++ recipesite/recipesite/settings.py
@@ -125,3 +125,10 @@ MEDIA_URL = '/media/'
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'recipesite_cache'),
+    }
+}
