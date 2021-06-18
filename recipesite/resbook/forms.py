c7b3cd2c86d7de10580687285ff1d31d9310cb1f
IrinaPanov <iwell1202@gmail.com>
2021-06-17 20:58:34 -0400

    About Us update

--- recipesite/resbook/forms.py
+++ recipesite/resbook/forms.py
@@ -50,5 +50,5 @@ class LoginUserForm(AuthenticationForm):
 class ContactForm(forms.Form):
     name = forms.CharField(label='Name', max_length=200)
     email = forms.EmailField(label='Email')
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
     captcha = CaptchaField()
